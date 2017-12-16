5bfc7b792b365de99f4be9d53490799cddbe7e8e
Раздоркина Екатерина <32110208+katorina@users.noreply.github.com>
2017-12-16 15:17:37 +0300

    Update Parse.kt

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -66,7 +66,7 @@ fun main(args: Array<String>) {
  * При неверном формате входной строки вернуть пустую строку
  */
 fun dateStrToDigit(str: String): String {
-    if (str[0] == ' ') return ""
+    if (str[0] == ' ' || str.isEmpty()) return ""
     var parts = str.split(' ')
     var res = ""
     val months = listOf<String>("", "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа",
